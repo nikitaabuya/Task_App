823dc91fb6f4493c52348e8ce1631dd78424174c
Nikita Joan Abuya <112301@STD.STRATHMORE.LOCAL>
2022-10-19 18:47:45 +0300

    v2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("I am a script");
+window.addEventListener('load', ()=>{
+
+const form = document.querySelector("#new-task-form");
+const input = document.querySelector('#new-task-input');
+const list_el = document.querySelector('#tasks');
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+})
+
+
+});
