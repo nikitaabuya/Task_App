57e6e2faab549ff168c67f0b54ded245f4ce63ad
Nikita Joan Abuya <112301@STD.STRATHMORE.LOCAL>
2022-10-19 18:56:18 +0300

    v2

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const list_el = document.querySelector('#tasks');
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
+
+    const task = input.value;
+    const task_el = document.createElement('div');
+    task_el.classList.add('task');
+
+    const task_content_el = document.createElement('div');
+    task_content_el.classList.add('content');
 })
 
 
